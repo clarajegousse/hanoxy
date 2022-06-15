7e1c6c71b3e784142fa793d0de1be9c26a4fc8ac
clarajegousse <clara.jegousse@gmail.com>
2022-06-15 21:12:38 +0100

    scheduler

---
+++ scheduler.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+#SBATCH -J scheduler
+#SBATCH -t 7-00:00:00
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -c 1
+#SBATCH -p normal
+#SBATCH --mem=1gb
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=cat3@hi.is
+
+cd /users/home/cat3/projects/hanoxy
+
+mkdir -p logs_slurm
+snakemake --cluster-config cluster.yaml --cluster 'sbatch -t {cluster.time} --mem={cluster.mem} -c {cluster.cpus} -o {cluster.output} -e {cluster.error} --mail-type {cluster.email_type} --mail-user {cluster.email}' -j 10
