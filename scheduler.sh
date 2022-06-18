3354dbc643ae670c8f95ceb53a6b504d4b228878
clarajegousse <clara.jegousse@gmail.com>
2022-06-18 08:45:54 +0100

    new scheduler script

--- scheduler.sh
+++ scheduler.sh
@@ -1,14 +1,30 @@
-#! /bin/bash
-#SBATCH -J scheduler
-#SBATCH -t 7-00:00:00
-#SBATCH -N 1
-#SBATCH -n 1
-#SBATCH -c 1
-#SBATCH -p normal
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=cat3@hi.is
-
-cd /users/home/cat3/projects/hanoxy
-
-mkdir -p logs_slurm
-snakemake --cluster-config cluster.yaml --cluster 'sbatch -t {cluster.time} -c {cluster.cpus} -o {cluster.output} -e {cluster.error} --mail-type {cluster.email_type} --mail-user {cluster.email}' -j 10
+if [ -z "$1" ]
+then
+    sbatch -J snake \
+    -o snake.log \
+    --wrap="snakemake --cluster-config cluster.yaml --cluster 'sbatch -t {cluster.time} -c {cluster.cpus} -o {cluster.output} -e {cluster.error} --mail-type {cluster.email_type} --mail-user {cluster.email}' -j 10 --latency-wait 60" \
+elif [ $1 = 'unlock' ]
+then
+    sbatch -J unlock \
+        -o dge.log \
+        --wrap="snakemake --unlock" \
+        --constraint="intel" \
+        --partition="normal" \
+        --mem="16G"
+elif [ $1 = "dryrun" ]
+then
+    sbatch -J dryrun \
+    -o dge.log \
+    --wrap="snakemake -n" \
+    --constraint="intel" \
+    --partition="normal" \
+    --mem="16G"
+elif [ $1 = "dag" ]
+then
+    sbatch -J dag \
+    -o snake.log \
+    --wrap="snakemake --dag > dag.dot" \
+    --constraint="intel" \
+    --partition="normal" \
+    --mem="16G"
+fi
