47695c6b5d516a949b7cb4992102790448424356
clarajegousse <clara.jegousse@gmail.com>
2022-06-18 11:57:48 +0100

    rename logs

--- scheduler.sh
+++ scheduler.sh
@@ -1,24 +1,23 @@
 if [ -z "$1" ]
 then
     sbatch -J snake \
-    -o snake.log \
     --wrap="snakemake --cluster-config cluster.yaml --cluster 'sbatch -t {cluster.time} -c {cluster.cpus} -o {cluster.output} -e {cluster.error} --mail-type {cluster.email_type} --mail-user {cluster.email}' -j 10 --latency-wait 60"
 elif [ $1 = 'unlock' ]
 then
     sbatch -J unlock \
-        -o snake.log \
+        -o snake-unlock.log \
         --wrap="snakemake --unlock" \
         --partition="normal"
 elif [ $1 = "dryrun" ]
 then
     sbatch -J dryrun \
-    -o snake.log \
+    -o snake-dryrun.log \
     --wrap="snakemake -n" \
     --partition="normal"
 elif [ $1 = "dag" ]
 then
     sbatch -J dag \
-    -o snake.log \
+    -o snake-dag.log \
     --wrap="snakemake --dag > dag.dot" \
     --partition="normal"
 fi
