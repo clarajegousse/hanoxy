bff1684bf93ce31876b72b3de059521caf99ef5f
clarajegousse <clara.jegousse@gmail.com>
2022-06-18 08:54:19 +0100

    fixes

--- scheduler.sh
+++ scheduler.sh
@@ -2,29 +2,23 @@ if [ -z "$1" ]
 then
     sbatch -J snake \
     -o snake.log \
-    --wrap="snakemake --cluster-config cluster.yaml --cluster 'sbatch -t {cluster.time} -c {cluster.cpus} -o {cluster.output} -e {cluster.error} --mail-type {cluster.email_type} --mail-user {cluster.email}' -j 10 --latency-wait 60" 
+    --wrap="snakemake --cluster-config cluster.yaml --cluster 'sbatch -t {cluster.time} -c {cluster.cpus} -o {cluster.output} -e {cluster.error} --mail-type {cluster.email_type} --mail-user {cluster.email}' -j 10 --latency-wait 60"
 elif [ $1 = 'unlock' ]
 then
     sbatch -J unlock \
-        -o dge.log \
+        -o snake.log \
         --wrap="snakemake --unlock" \
-        --constraint="intel" \
-        --partition="normal" \
-        --mem="16G"
+        --partition="normal"
 elif [ $1 = "dryrun" ]
 then
     sbatch -J dryrun \
-    -o dge.log \
+    -o snake.log \
     --wrap="snakemake -n" \
-    --constraint="intel" \
-    --partition="normal" \
-    --mem="16G"
+    --partition="normal"
 elif [ $1 = "dag" ]
 then
     sbatch -J dag \
     -o snake.log \
     --wrap="snakemake --dag > dag.dot" \
-    --constraint="intel" \
-    --partition="normal" \
-    --mem="16G"
+    --partition="normal"
 fi
